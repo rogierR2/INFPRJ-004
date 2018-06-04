61dcc316ae3528a243118a161faec9033b97894f
Rogier <rogier-RC@live.nl>
2018-06-04 03:01:08 +0200

    App, scrum en alles

---
+++ QuickMath/QuickMath/obj/Debug/android/src/md5f6ad501d15e0e8ad1d0373d2f90d1e7e/MainActivity.java
@@ -0,0 +1,47 @@
+package md5f6ad501d15e0e8ad1d0373d2f90d1e7e;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("QuickMath.MainActivity, QuickMath", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("QuickMath.MainActivity, QuickMath", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
